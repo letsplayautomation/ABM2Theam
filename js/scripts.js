ab0d8e2e32a7e81b56d2ca859959137464c24b22
letsplayautomation <121675780+letsplayautomation@users.noreply.github.com>
2023-05-23 18:48:09 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,9 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrink')
         } else {
-            navbarCollapsible.classList.remove('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrink')
         }
 
     };
